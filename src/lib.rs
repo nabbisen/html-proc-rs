e6129a02cfa32760b2dea3ebcee1fa6d15cb18f8
nabbisen <nabbisen@scqr.net>
2024-06-28 12:14:25 +0900

    docs.rs: add lib comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+//! HTML processors as utils.
+//! Each function is offered as a single `feature`, so the dependencies are kept small. (`omit_enclosure` which is used as document outline formatter is exception.)
+
 // #[cfg(feature = "omit_attr")]
 pub mod omit_attr;
 pub mod omit_enclosure;
